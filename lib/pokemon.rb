4f7ed29b90ba8536efbe6b9820d84c9cebef6969
Zachary Schulz <zjschulz@gmail.com>
2019-11-18 23:58:24 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -9,8 +9,21 @@ class Pokemon
     @id = id
   end
   
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO songs (name, album)
+        VALUES (?, ?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.album)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+    end
+  end
+  
   def update
-    sql = "UPDATE songs SET name = ?, album = ? WHERE name = ?"
+    sql = "UPDATE pokemon SET name = ?, type = ? WHERE name = ?"
     DB[:conn].execute(sql, self.name, self.album, self.name)
   end
   

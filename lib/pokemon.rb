732a1a433c1f35484cc9322ec670731d3fa91697
Zachary Schulz <zjschulz@gmail.com>
2019-11-18 23:57:37 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -9,4 +9,9 @@ class Pokemon
     @id = id
   end
   
+  def update
+    sql = "UPDATE songs SET name = ?, album = ? WHERE name = ?"
+    DB[:conn].execute(sql, self.name, self.album, self.name)
+  end
+  
 end

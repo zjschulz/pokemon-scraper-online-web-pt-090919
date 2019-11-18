96882685ff4681a313b550ca70fc71c64e1b5f6a
Zachary Schulz <zjschulz@gmail.com>
2019-11-18 23:39:15 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,12 @@
 class Pokemon
+  
+  attr_accessor :id, :name, :type, :db
+  
+  def initialize(id = nil, name, type, db)
+    @name = name
+    @type = type
+    @db = db
+    @id = id
+  end
+  
 end
